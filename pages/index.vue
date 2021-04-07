eba868819e7038070c584bfbf3cbd74d66be58dd
NullPointer <peetza2568@gmail.com>
2021-04-07 19:35:41 +0700

    Update index.vue
    - Add animations

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-<section class=" max-w-7xl leading-snug mx-auto text-8xl font-bold flex flex-col space-y-8 justify-center items-center w-full min-h-screen text-gray-800 text-center">
-<span>AKADO</span>
-<small class="animate-pulse w-full text-gray-700 text-sm font-semibold">
+<section class="animate-bounce max-w-7xl leading-snug mx-auto text-8xl font-bold flex flex-col space-y-8 justify-center items-center w-full min-h-screen text-gray-800 text-center">
+<span class="animate-pulse">AKADO</span>
+<small class="w-full animate-bounce text-gray-700 text-sm font-semibold">
   COMING SOON
 </small>
 </section>
